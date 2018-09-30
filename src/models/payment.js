d12ee24867fbed2d93c72cc5781bd95dacefd2e4
Fredrik Engberg <fredrik.w.engberg@gmail.com>
2018-09-30 15:20:48 +0200

    Model updates while developing the Crediting Service. - Rule to prevent payment from being credited more than once. - Added payment status. - Added support for internal message in UserError. - Updated documentation. - Bumped version.

--- src/models/payment.js
+++ src/models/payment.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
 const paymentSchema = new mongoose.Schema({
+        status: { type: String, enum: ['INCOMING', 'PROCESSING_PENDING', 'PROCESSED', 'PROCESSSING_FAILED'], required: true, default: 'INCOMING' },
         payment_system: { type: String, enum: ['stellar', 'coinpayments'], required: true},
         currency: { type: String, enum: ['XLM', 'ETH', 'BTC'], required: true },
         amount: { type: Number, required: true },
         source_ref: { type: String, required: true },
         tx_id: { type: String, required: true },
         tx_timestamp: { type: Date, required: true },
-        tx_info: { type: mongoose.Schema.Types.Mixed, default: null }
+        tx_info: { type: mongoose.Schema.Types.Mixed, default: null },
+        status_message: { type: String, required: false }
     },
     { timestamps: true }
 );
