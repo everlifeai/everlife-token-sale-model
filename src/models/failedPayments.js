b27d71bc8caa86ff55ec7645ecee8f6820026886
Michiel Mulders <michielmulders1@gmail.com>
2018-10-10 15:31:55 +0200

    Update failedPayments - add message field

--- src/models/failedPayments.js
+++ src/models/failedPayments.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const failedPaymentSchema = new mongoose.Schema({
         issue_to: { type: String, required: true },
         email: { type: String, required: true },
-        source_ref: { type: String, required: true }
+        source_ref: { type: String, required: true },
+        reason: { type: String, required: true }
     },
     { timestamps: true }
 );
