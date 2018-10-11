6654224be4b7e9de714e188a2b2f3f4b7a9f0927
michielmulders <michielmulders1@gmail.com>
2018-10-11 18:49:43 +0200

    collection name enforce

--- src/models/archivedPayments.js
+++ src/models/archivedPayments.js
@@ -4,7 +4,8 @@ const archivedPaymentSchema = new mongoose.Schema({
         issue_to: { type: String, required: true },
         ever: { type: Number, required: true },
     },
-    { timestamps: true }
+    { timestamps: true },
+    { collection: 'issuing_archived' }
 );
 
 module.exports = mongoose.model('issuing_archived', archivedPaymentSchema);
--- src/models/creditedPaymentObjects.js
+++ src/models/creditedPaymentObjects.js
@@ -4,7 +4,8 @@ const creditedPaymentObjectSchema = new mongoose.Schema({
         issue_to: { type: String, required: true },
         ever: { type: Number, required: true },
     },
-    { timestamps: true }
+    { timestamps: true },
+    { collection: 'issuing_pending' }
 );
 
 module.exports = mongoose.model('issuing_pending', creditedPaymentObjectSchema);
